8f4cecad642eecf011cf9c64a2dcf43a43da9df0
Adeyemi Akinyemi <89089354+AdeDevs@users.noreply.github.com>
2023-01-15 19:36:36 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app-container">
+    <HomePage msg="With Alt" />
+  </div>
+</template>
+
+<script>
+import HomePage from "./components/HomePage.vue";
+export default {
+  name: "App",
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.app-container {
+  margin: 5px auto;
+  width: 98%;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Data Fetching {{ msg }}</h2>
+    <p class="joke">{{ data.joke }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      data: "",
+    };
+  },
+  mounted() {
+    fetch("https://icanhazdadjoke.com/search?term=dog", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => (this.data = data));
+  },
+};
+</script>
